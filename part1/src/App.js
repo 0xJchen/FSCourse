d58ae94aa56fcba76bb9abc6784015c8d4466b10
0xJChen <wangjch@umich.edu>
2022-11-14 00:40:28 -0500

    update part1

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,89 +1,128 @@
-const Header = (props) =>{
-  return(
-    <div>
-      <p>
-        {props.course}
-      </p>
-    </div>
-  )
+import { useState } from "react";
+
+function randomNum(minNum, maxNum) {
+  switch (arguments.length) {
+    case 1:
+      return parseInt(Math.random() * minNum + 1, 10);
+      break;
+    case 2:
+      return parseInt(Math.random() * (maxNum - minNum + 1) + minNum, 10);
+      break;
+    default:
+      return 0;
+      break;
+  }
 }
 
-const Part = (props) =>{
-  return(
-    <div>
-      <p>
-        {props.name}
-      </p>
-    </div>
-  )
-}
+const Display = (props) => <div>{props.value}</div>;
 
-const Content = (props) =>{
-  return(
-    <div>
-      <p>
-        <Part name={props.name1}/>
-        <Part name={props.name2}/>
-      </p>
-    </div>
-  )
-}
+const Button = (props) => (
+  <button onClick={props.handleClick}>{props.text}</button>
+);
 
+const StatisticLine = (props) => {
+  return (
+    <>
+      <td>{props.text}</td>
+      <td>{props.value}</td>
+    </>
+  );
+};
 
-
-const Total = (props) =>{
-  return(
-    <div>
-      <p>
-        Number of exercises  {props.num1} + {props.num2}
-      </p>
-    </div>
-  )
-}
+const Statistics = (props) => {
+  const sum = props.bad + props.good + props.neutral;
+  if (sum === 0) {
+    return (
+      <>
+        <p>no data available</p>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <table>
+          <tr>
+            <StatisticLine text="good" value={props.good} />
+          </tr>
+          <tr>
+            <StatisticLine text="neutral" value={props.neutral} />
+          </tr>
+          <tr>
+            <StatisticLine text="bad" value={props.bad} />
+          </tr>
+        </table>
+        {/* <p>good {props.good}</p>
+      <p>neutral {props.neutral}</p>
+      <p>bad {props.bad}</p>
+      <p>all {props.bad+props.good+props.neutral}</p>
+      <p>average {(props.bad+props.good+props.neutral)/4}</p>
+      <p>positive {props.good/(props.bad+props.good+props.neutral)}</p> */}
+      </>
+    );
+  }
+};
 
 const App = () => {
-  // const-definitions
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [selected, setSelected] = useState(0);
+
+  const anecdotes = [
+    "If it hurts, do it more often",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients",
+  ];
+
+  const setToGood = (newValue) => {
+    console.log("value now", newValue);
+    setGood(newValue);
+  };
+
+  const setToNeutral = (newValue) => {
+    console.log("value now", newValue);
+    setNeutral(newValue);
+  };
+
+  const setToBad = (newValue) => {
+    console.log("value now", newValue);
+    setBad(newValue);
+  };
+
+  const setToSelect = (newValue) => {
+    console.log("value now", newValue);
+    setBad(newValue);
+  };
 
   return (
     <div>
-      <Header course={course} />
-      <Content name1={part1} name2={part2} />
-      <Total  num1={exercises1} num2={exercises2}  />
+      {/* <Display value={value} /> */}
+      <p>
+        <b>give feedback</b>
+      </p>
+      <Button handleClick={() => setToGood(good + 1)} text="good" />
+      <Button handleClick={() => setToNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setToBad(bad + 1)} text="bad" />
+      <p>
+        <b>Statistics</b>
+      </p>
+      <Statistics good={good} bad={bad} neutral={neutral} />
+      {/* part2 */}
+      <p>
+        <b>Today Random</b>
+      </p>
+      {anecdotes[randomNum(0, 6)]}
+      <Button handleClick={() => setToGood(good + 1)} text="good" />
+
+      <p>has {selected} votes.</p>
+      <p>
+        <b>Most popular</b>
+      </p>
     </div>
-  )
-}
-
-// const App = () => {
-//   const course = 'Half Stack application development'
-//   const part1 = 'Fundamentals of React'
-//   const exercises1 = 10
-//   const part2 = 'Using props to pass data'
-//   const exercises2 = 7
-//   const part3 = 'State of a component'
-//   const exercises3 = 14
-
-//   return (
-//     <div>
-//       <h1>{course}</h1>
-//       <p>
-//         {part1} {exercises1}
-//       </p>
-//       <p>
-//         {part2} {exercises2}
-//       </p>
-//       <p>
-//         {part3} {exercises3}
-//       </p>
-//       <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-//     </div>
-//   )
-// }
-
-export default App
+  );
+};
+export default App;
